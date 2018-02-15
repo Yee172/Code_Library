2af086dc65ec979a438dbc1baa4c3dc323c12c5a
Qihao Ye <11610812@mail.sustc.edu.cn>
2018-02-15 14:32:29 +0800

    update

--- 934/Problem_934B.py
+++ 934/Problem_934B.py
@@ -1,12 +1,2 @@
-pt = lambda *a, **k: print(*a, **k, flush=True)
 k = int(input())
-if k > 36:
-    pt(-1)
-else:
-    while k:
-        if k & 1:
-            pt('4', end='')
-            k -= 1
-        else:
-            pt('8', end='')
-            k -= 2
+print(-1 if k > 36 else k // 2 * '8' + k % 2 * '6')
