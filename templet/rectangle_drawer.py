68d5cc88e47d9ee065777008860b5f6d09a049b7
Qihao Ye <11610812@mail.sustc.edu.cn>
2018-04-13 11:53:09 +0800

    update

---
+++ templet/rectangle_drawer.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+ax = plt.axes()
+ax.xaxis.set_major_locator(plt.MultipleLocator(1.0))
+ax.yaxis.set_major_locator(plt.MultipleLocator(1.0))
+while 1:
+    rd = input()
+    if rd == '0':
+        break
+    x1, y1, x2, y2 = map(int, rd.split())
+    ax.add_patch(patches.Rectangle((x1, y1), x2 - x1, y2 - y1, fill=False, edgecolor='black'))
+plt.plot(np.arange(0, 0))
+plt.grid()
+plt.grid(color='b', linewidth='0.3' ,linestyle='--')
+plt.show()
