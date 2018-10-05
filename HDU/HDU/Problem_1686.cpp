cb846f11c54fe271b1cd3dc252720d59f87fef97
Qihao Ye <11610812@mail.sustc.edu.cn>
2018-10-06 02:23:03 +0800

    Update

---
+++ HDU/HDU/Problem_1686.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#pragma optimize("Ofast")
+using namespace std;
+
+#ifdef Yee_172
+#include "template/tools/local_test.hpp"
+#else
+#define _______ ;
+#define _debug(...) ;
+#endif
+
+typedef long long ll;
+#define rep(i, n) for (int (i) = 0; (i) < (n); ++(i))
+#define _rep(i, n) for (int (i) = 1; (i) <= (n); ++(i))
+
+namespace KMP
+{
+    const int MAXN = 1000050;
+    const int MAXM = 10050;
+    char a[MAXN];
+    int len_a;
+    int nt[MAXN];
+    char b[MAXM];
+    int len_b;
+
+    inline void get_next()
+    {
+        nt[0] = 0;
+        for (int q = 1, k = 0; q < len_b; ++q)
+        {
+            while (k > 0 && b[q] != b[k]) k = nt[k - 1];
+            if (b[q] == b[k]) ++k;
+            nt[q] = k;
+        }
+    }
+
+    inline int kmp() // return index starts from 0, -1 means none
+    {
+        int res = 0;
+        for (int i = 0, k = 0; i < len_a; ++i)
+        {
+            while (k > 0 && b[k] != a[i]) k = nt[k - 1];
+            if (b[k] == a[i]) ++k;
+            if (k == len_b)
+            {
+                k = nt[k - 1];
+                ++res;
+            }
+        }
+        return res;
+    }
+}
+using namespace KMP;
+
+int main()
+{
+    int t;
+    for (scanf("%d", &t); t--; )
+    {
+        scanf("%s%s", b, a);
+        len_a = (int) strlen(a);
+        len_b = (int) strlen(b);
+        get_next();
+        printf("%d\n", kmp());
+    }
+    return 0;
+}
