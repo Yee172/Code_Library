bce5159de55c474971c4824f09dd5aad470ef2d8
Qihao Ye <11610812@mail.sustc.edu.cn>
2018-12-08 21:23:38 +0800

    Create Problem_500.py

---
+++ ProjectEuler/Problem_500.py
@@ -0,0 +1,39 @@
+# Find the smallest number with 2^{500500} divisors.
+# Give your answer modulo 500500507.
+# ----------------------------------------------------
+# Analysis: greedy algorithm
+
+from queue import PriorityQueue
+
+NEED = 500500
+MOD = 500500507
+
+print('Sieving prime numbers...')
+MAXN = 8005000
+prime = []
+vis = [False] * MAXN
+vis[0] = True
+vis[1] = True
+for i in range(2, MAXN):
+    if not vis[i]:
+        prime.append(i)
+    for x in prime:
+        if i * x >= MAXN:
+            break
+        vis[i * x] = True
+        if not i % x:
+            break
+print('Prime numebr generated successfully.')
+
+res = 1
+q = PriorityQueue()
+q.put((2, 0, 1))
+for _ in range(NEED):
+    x, y, z = q.get()
+    if z == 1:
+        q.put((prime[y + 1], y + 1, 1))
+    q.put((x * x, y, z << 1))
+    res *= x % MOD
+    res %= MOD
+
+print(res)
