110f08b927c5df10793be1041c7a1b9821541ee7
Qihao Ye <11610812@mail.sustc.edu.cn>
2018-12-12 13:57:29 +0800

    Create Problem_3.py

---
+++ ProjectEuler/Problem_3.py
@@ -0,0 +1,29 @@
+# What is the largest prime factor of
+# the number 600851475143?
+# ----------------------------------------------------
+# Analysis: brute force
+
+print('Sieving prime numbers...')
+MAXN = 500000
+prime = []
+vis = [False] * MAXN
+vis[0] = True
+vis[1] = True
+for i in range(2, MAXN):
+    if not vis[i]:
+        prime.append(i)
+    for x in prime:
+        if i * x >= MAXN:
+            break
+        vis[i * x] = True
+        if not i % x:
+            break
+print('Prime numebr generated successfully.')
+
+x = 600851475143
+for p in prime:
+    while not x % p:
+        x /= p
+    if x == 1:
+        print(p)
+        break
