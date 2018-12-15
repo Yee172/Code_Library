fb048a4a69efecc214142149045d1823a24c3fa7
Qihao Ye <11610812@mail.sustc.edu.cn>
2018-12-16 05:14:48 +0800

    Create Problem_7.py

---
+++ ProjectEuler/Problem_7.py
@@ -0,0 +1,22 @@
+# What is the 10001st prime number?
+# ----------------------------------------------------
+# Analysis: Euler sieve
+
+print('Sieving prime numbers...')
+MAXN = 1000100
+prime = []
+vis = [False] * MAXN
+vis[0] = True
+vis[1] = True
+for i in range(2, MAXN):
+    if not vis[i]:
+        prime.append(i)
+    for x in prime:
+        if i * x >= MAXN:
+            break
+        vis[i * x] = True
+        if not i % x:
+            break
+print('Prime numebr generated successfully.')
+
+print(prime[10000])
