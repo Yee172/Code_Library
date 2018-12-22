4e74acb84ae0ae9d856b152d5d55306ff5dd561b
Qihao Ye <11610812@mail.sustc.edu.cn>
2018-12-22 14:06:07 +0800

    Create Problem_10.py

---
+++ ProjectEuler/Problem_10.py
@@ -0,0 +1,22 @@
+# Find the sum of all the primes below two million.
+# ----------------------------------------------------
+# Analysis: Euler sieve
+
+print('Sieving prime numbers...')
+MAXN = 2000000
+prime = []
+vis = [False] * MAXN
+vis[0] = True
+vis[1] = True
+for i in range(2, MAXN):
+    if not vis[i]:
+        prime.append(i)
+    for x in prime:
+        if i * x >= MAXN:
+            break
+        vis[i * x] = True
+        if not i % x:
+            break
+print('Prime numebr generated successfully.')
+
+print(sum(prime))
