8ed858278466615461416357a1df53bea8de67f1
Qihao Ye <11610812@mail.sustc.edu.cn>
2018-12-24 08:18:43 +0800

    Create Problem_12.py

---
+++ ProjectEuler/Problem_12.py
@@ -0,0 +1,46 @@
+# What is the value of the first triangle number
+# to have over five hundred divisors?
+# ----------------------------------------------------
+# Analysis: brute force
+
+num = 500
+
+print('Sieving prime numbers...')
+MAXN = 10 ** 6
+prime = []
+vis = [False] * MAXN
+vis[0] = True
+vis[1] = True
+for i in range(2, MAXN):
+    if not vis[i]:
+        prime.append(i)
+    for x in prime:
+        if i * x >= MAXN:
+            break
+        vis[i * x] = True
+        if not i % x:
+            break
+print('Prime numebr generated successfully.')
+
+def get_triangle_num(x):
+    return x * (x + 1) >> 1
+
+def check(x):
+    r = 1
+    for p in prime:
+        if p * p >= x:
+            break
+        t = 0
+        while not x % p:
+            x /= p
+            t += 1
+        r *= t + 1
+    if x > 1:
+        r <<= 1
+    return r > num
+
+for i in range(MAXN):
+    x = get_triangle_num(i)
+    if check(x):
+        print(x)
+        break
