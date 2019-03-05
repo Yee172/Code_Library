0029069bcc403920d7cb927c18c74058a021bd57
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-03-05 17:58:41 +0800

    Update Problem_0659

--- ProjectEuler/Problem_0659.py
+++ ProjectEuler/Problem_0659.py
@@ -1,8 +1,8 @@
-# For the first one hundred natural numbers, find the
-# total of the digital sums of the first one hundred
-# decimal digits for all the irrational square roots.
+# Let P(k) be the largest prime that divides any two
+# successive terms of the sequence n^{2} + k^{2}.
+# Find the last 18 digits of \sum_{k = 1}^{10000000} P(k)
 # ----------------------------------------------------
-# Analysis: Pollard’s rho algorithm
+# Analysis: brute force
 #           p | n^{2} + k^{2}
 #           p | (n + 1)^{2} + k^{2}
 #              => p | 2 n + 1
@@ -14,45 +14,30 @@
 #           p | 2 n + 1
 #              => p | 4 n + 2
 #              => p | 1 + 4 k^{2}
+#           We only have to find the sum of the largest prime factor of 1 + 4 k^{2}
+#               1 + 4 k^{2} | 1 + 4 (k + m)^{2}
+#           <=> 1 + 4 k^{2} | 1 + 4 k^{2} + 4 m (m + 2 k)
+#           <=> 1 + 4 k^{2} | m (m + 2 k)
+#           <=> 1 + 4 k^{2} | m or 1 + 4 k^{2} | m + 2 k
+#           <=> m = n (1 + 4 k^{2}) or m = n (1 + 4 k^{2}) - 2 k, \forall n \in \N
+#              => 1 + 4 k^{2} | 1 + 4 (n (1 + 4 k^{2}) \pm k)^{2}, \forall n \in \N
 
-print('Sieving prime numbers...')
-MAXN = 10 ** 8
-prime = []
-vis = [False] * MAXN
-vis[0] = True
-vis[1] = True
-for i in range(2, MAXN):
-    if not vis[i]:
-        prime.append(i)
-    for x in prime:
-        if i * x >= MAXN:
-            break
-        vis[i * x] = True
-        if not i % x:
-            break
-print('Prime number generated successfully.')
+MAXN = 10 ** 7 + 1
 
-def largest_prime(n):
-    if n < MAXN:
-        if not vis[n]:
-            return n
-    r = 0
-    for p in prime:
-        if p * p > n:
-            break
-        if not n % p:
-            r = p
-            while not n % p:
-                n //= p
-    if n > 1:
-        r = n
-    return r
+candidates = [k ** 2 << 2 | 1 for k in range(MAXN)]
+largest_prime_factor = [0] * MAXN
 
-MOD = 10 ** 18
-r = 0
-for k in range(1, 10 ** 7 + 1):
-    if not k % 1000:
-        print(k)
-    r += largest_prime(k ** 2 << 2 | 1)
-    r %= MOD
-print(r)
+for k in range(1, MAXN):
+    x = candidates[k]
+    if x <= 2 * k:
+        continue
+    for i in range(k, MAXN, x):
+        largest_prime_factor[i] = max(largest_prime_factor[i], x)
+        while not candidates[i] % x:
+            candidates[i] //= x
+    for i in range(x - k, MAXN, x):
+        largest_prime_factor[i] = max(largest_prime_factor[i], x)
+        while not candidates[i] % x:
+            candidates[i] //= x
+
+print(sum(largest_prime_factor) % 10 ** 18)
