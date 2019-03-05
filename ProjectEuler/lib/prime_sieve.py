8840cd1cfa898c4f0e0b2ceee2ccdc6dfa098bef
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-03-05 10:39:12 +0800

    Create prime_sieve.py

---
+++ ProjectEuler/lib/prime_sieve.py
@@ -0,0 +1,36 @@
+def prime_sieve(MAXN=10**7, info=True):
+    """Return a list of prime up to MAXN
+    if bitarray is installed:
+        Sieve of Eratosthenes (Learnt from code of epiclolz)
+        PS: faster because of the operation of bitarray
+    else:
+        Sieve of Euler in normal way
+    """
+    if info:
+        print('Sieving prime numbers...')
+    try:
+        from bitarray import bitarray
+        is_prime = bitarray(MAXN + 1 >> 1)
+        is_prime.setall(1)
+        is_prime[0] = 0
+        for i in range(1, int(MAXN ** .5)):
+            if is_prime[i]:
+                is_prime[2 * i ** 2 + 2 * i::2 * i + 1] = 0
+        prime = [2] + [i * 2 + 1 for i in is_prime.search(bitarray([1]))]
+    except:
+        prime = []
+        vis = [False] * MAXN
+        vis[0] = True
+        vis[1] = True
+        for i in range(2, MAXN):
+            if not vis[i]:
+                prime.append(i)
+            for x in prime:
+                if i * x >= MAXN:
+                    break
+                vis[i * x] = True
+                if not i % x:
+                    break
+    if info:
+        print('Prime number generated successfully.')
+    return prime
