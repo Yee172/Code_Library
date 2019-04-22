867eaf0d893326a2528837d73a71b89d965f9175
Qihao Ye <11610812@mail.sustc.edu.cn>
2019-04-23 07:40:27 +0800

    Create Problem_0145.py

---
+++ ProjectEuler/Problem_0145.py
@@ -0,0 +1,8 @@
+# Some positive integers n have the property that the
+# sum [n + reverse(n)] consists entirely of odd
+# (decimal) digits.
+# How many reversible numbers are there below one-billion?
+# ----------------------------------------------------
+# Analysis: brute force
+
+print(0 + 20 + 20 * 5 + 20 * 30 + 0 + 20 * 30 ** 2 + 20 * 25 * 20 * 5 + 20 * 30 ** 3 + 0)
